998c550a06ba33002962e477211260596447d730
Enoch Oduyale <oduyaleenoch@gmail.com>
2020-10-07 17:52:36 -0700

    fixing scroll

---
+++ app.js
@@ -0,0 +1,25 @@
+window.sr = ScrollReveal();
+
+
+
+
+sr.reveal('.take-exam', {
+    duration: 2000,
+    origin: 'left',
+    distance: "200px",
+    viewFactor:0.2,
+});
+
+sr.reveal('.sec', {
+    duration: 2000,
+    origin: 'left',
+    distance: "200px",
+    viewFactor:0.2,
+});
+
+sr.reveal('.take-exam-img', {
+    duration: 2000,
+    origin: 'right',
+    distance: "200px",
+    viewFactor:0.2,
+});
